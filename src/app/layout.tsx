afaee87fe77f050ad4ec3522fa6f231979d7b00a
wahibonae <wahibabkari@gmail.com>
2025-01-24 22:01:26 +0100

    font update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: "Code To Prompt",
@@ -17,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`${spaceGrotesk.className} ${inter.className}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"

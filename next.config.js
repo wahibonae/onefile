7ab2c0076e0f5a50f75190ad42d3efa0611bf374
wahibonae <wahibabkari@gmail.com>
2025-07-08 13:34:38 +0100

    cloudflare fix

--- next.config.js
+++ next.config.js
@@ -1,52 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/seo-card.jpg',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'image/jpeg',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type',
-          },
-        ],
-      },
-      {
-        source: '/(.*\\.(png|jpg|jpeg|gif|svg|ico|webp))',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'image/*',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-        ],
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig 
